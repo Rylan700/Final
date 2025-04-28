503c4847368f268a6ca154bbb8aa1b064683b7f0
Rylan700 <rw0971064@otc.edu>
2025-04-28 08:36:36 -0500

    Initial Commit

---
+++ Final/FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalApp.swift
+//  Final
+//
+//  Created by WADE, RYLAN on 4/28/25.
+//
+
+import SwiftUI
+
+@main
+struct FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
